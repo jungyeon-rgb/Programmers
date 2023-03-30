d619ee346314b32588b4e935fb7f433e9970a7ef
김정연 <119380048+jungyeon-rgb@users.noreply.github.com>
2023-03-30 13:23:19 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.09 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.js
+++ 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.js
@@ -6,9 +6,13 @@ function solution(my_string) {
     if (isNaN(Number(tempString[i]))) {
       continue;
     } else if (tempString[i-1] === "+" || i === 0) {
+        // tempString의 i번째 값이 숫자이면서 이전 값이 "+"이거나 0이면
       newArr += parseInt(tempString[i]);
+        // newArr에 tempString[i]를 더한다
     } else if (tempString[i-1] === "-") {
+        // tempString의 i번째 값이 숫자이면서 이전 값이 "-"이면
       newArr -= parseInt(tempString[i]);
+        // newArr에 tempString[i]를 뺀다
     }
   }
   return newArr;
