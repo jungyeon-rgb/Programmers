f5722dd683d6ae629242c39a441dcd3054f993e9
김정연 <119380048+jungyeon-rgb@users.noreply.github.com>
2023-04-06 10:18:47 +0900

    [level 0] Title: 직사각형 넓이 구하기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.js
+++ 프로그래머스/lv0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.js
@@ -1,7 +1,7 @@
 function solution(dots) {
-  const x = dots.map(dot => dot[0]);
-  const y = dots.map(dot => dot[1]);
-  const width = Math.max(...x) - Math.min(...x);
-  const height = Math.max(...y) - Math.min(...y);
+  const x = dots.map(dot => dot[0]); // dots 배열의 각 요소에서 x 좌표를 추출하여 배열 x에 저장
+  const y = dots.map(dot => dot[1]); // dots 배열의 각 요소에서 y 좌표를 추출하여 배열 y에 저장
+  const width = Math.max(...x) - Math.min(...x); // 배열 x에서 가장 큰 값과 작은 값을 구한 후, 차이를 계산하여 가로 길이를 구함
+  const height = Math.max(...y) - Math.min(...y); // 배열 y에서 가장 큰 값과 작은 값을 구한 후, 차이를 계산하여 세로 길이를 구함
   return width * height;
 }
