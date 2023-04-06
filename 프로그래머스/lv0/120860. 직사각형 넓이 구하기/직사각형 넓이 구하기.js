dea0591f740c978ac43e5e6023a381537666792f
김정연 <119380048+jungyeon-rgb@users.noreply.github.com>
2023-04-06 09:36:52 +0900

    [level 0] Title: 직사각형 넓이 구하기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.js
@@ -0,0 +1,7 @@
+function solution(dots) {
+  const x = dots.map(dot => dot[0]);
+  const y = dots.map(dot => dot[1]);
+  const width = Math.max(...x) - Math.min(...x);
+  const height = Math.max(...y) - Math.min(...y);
+  return width * height;
+}
