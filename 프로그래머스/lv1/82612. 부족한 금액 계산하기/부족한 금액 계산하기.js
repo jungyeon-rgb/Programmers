18335588aac490001b80976d040c7cacef265545
김정연 <119380048+jungyeon-rgb@users.noreply.github.com>
2023-06-16 22:16:32 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
@@ -0,0 +1,9 @@
+function solution(price, money, count){
+    let totalCost = 0;
+    
+    for(let i=1; i<=count; i++){
+        totalCost += price * i;
+    }
+    
+    return totalCost > money ? totalCost - money : 0;
+}
