e9b2ab6c7c5bd602b9fea5762724970c505f67d1
김정연 <119380048+jungyeon-rgb@users.noreply.github.com>
2023-08-17 16:11:56 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 620 ms, Memory: 102864 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.js
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.js
@@ -1,46 +1,31 @@
-const sol = (input) => {
-    const N = +input[0];
-    input = input.slice(1);
-    const tree = Array.from({ length: N+1}, () => new Array());
-    input.map((val) => {
-        const [from,to] = val.split(" ").map(Number);
-        tree[from].push(to);
-        tree[to].push(from); // 무방향 트리이므로 A->B, B->A 정보를 모두 넣어준다.
-    });
-    
-    let check = Array.from({ length: N+1}, () => 0);
-    function bfs() {
-        const queue = [];
-        check[1] = 1;
-        for (let next of tree[1]) { // next(child) 노드 값의 인덱스에 1(부모 노드)값을 넣어주고, 큐에 넣어준다.
-            check[next] = 1;
-            queue.push(next);
-        }
-        while(queue.length) {
-            const node = queue.shift();
-            for (let next of tree[node]) { // 노드를 순회하며, 방문한 노드이면 건너뛴다.
-                if(check[next]) continue;
-                check[next] = node; // 위와 같이 next 인덱스에는 node(부모 노드)값을 넣는다.
-                queue.push(next);
-            }
+const fs = require('fs');
+const input = fs.readFileSync('dev/stdin').toString().trim().split('\n');
+
+const N = Number(input[0])
+
+const graph = []
+const visited = Array(N+1).fill(0)
+const answer = Array(N+1)
+for(let i=0; i<=N; i++) {
+    graph[i] = []
+}
+
+for(let i=1;i<N;i++) {
+    const [from,to] = input[i].split(' ').map(Number)
+    graph[from].push(to)
+    graph[to].push(from)
+}
+
+function dfs(s) {
+    visited[s] = 1
+    for(let n of graph[s]) {
+        if(!visited[n]) {
+            answer[n] = s
+            dfs(n)
         }
     }
-    bfs();
-    
-    check = check.slice(2);
-    let result = "";
-    check.map((v) => (result += `${v}\n`)); // 체크 배열의 2번 인덱스(2번 노드)부터 출력한다.
-    return result;
-};
+}
 
-// 백준에서 입력을 받는 코드
-const input = [];
-require("readline")
-    .createInterface(process.stdin, process.stdout)
-    .on("line", (line) => {
-        input.push(line);
-})
-    .on("close", () => {
-        console.log(sol(input));
-        process.exit();
-});
+dfs(1)
+let result = answer.slice(2).join('\n')
+console.log(result)
