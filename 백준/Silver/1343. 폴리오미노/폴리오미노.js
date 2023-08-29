194e53895053f004aacb6679b314dc0a485a30b1
김정연 <119380048+jungyeon-rgb@users.noreply.github.com>
2023-08-29 16:26:48 +0900

    [Silver V] Title: 폴리오미노, Time: 140 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split('\n').map(line => line.trim());
+
+// AAAA, BB만 가능하니까 4,2길이가 아니면 -1
+// .기준으로 문자열 나누기
+// AAAA, BB 대체할 수 있게 4글자, 2글자 찾아서 바꾸기
+
+function solve(board) {
+  board = board.split(".");
+  let result = '';
+ 
+  for (let i = 0; i < board.length; i++) {
+    let length = board[i].length;
+ 
+    if ((board[i].length % 4) % 2 !== 0) {
+      return "-1";
+    }
+ 
+    const first = Math.floor(board[i].length / 4);
+    const second = Math.floor((board[i].length % 4) / 2);
+ 
+    board[i] = "AAAA".repeat(first) + "BB".repeat(second);
+  }
+ 
+  return board.join(".");
+}
+ 
+console.log(solve(input[0]));
