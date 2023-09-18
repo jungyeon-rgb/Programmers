6308445a667ab668e72cb42551154d263ac30ef4
김정연 <119380048+jungyeon-rgb@users.noreply.github.com>
2023-09-18 17:44:39 +0900

    [Silver III] Title: 블로그, Time: 292 ms, Memory: 45240 KB -BaekjoonHub

---
+++ 백준/Silver/21921. 블로그/블로그.js
@@ -0,0 +1,33 @@
+let file = require('fs').readFileSync('/dev/stdin');
+let input = file.toString().split('\n');
+
+let [n,x] = input[0].split(" ").map(Number);
+let arr = [0, ...input[1].split(" ").map(Number)];
+
+let sum = 0;
+for (let i = 1; i <= n; i++) {
+    // 1부터 X번째 날의 방문자 수의 합
+    if (i<=x) sum += arr[i]; // 첫 번째 윈도우(구간)에 대해서 합 구함
+}
+let maxSum = sum; // 가장 큰 합
+let count = 1; // 기간의 개수
+
+// 슬라이딩 윈도우
+let left = 1;
+let right = x;
+while(true) { // 윈도우 오른쪽 한 칸 이동
+    left +=1;
+    right += 1;
+    if (right > n) break;
+    sum = sum + arr[right] - arr[left - 1]; // 합을 계산하여 정답 갱신
+    if (maxSum == sum) count += 1; // 최대값이 현재 합이랑 동일하면 동일한 구간이 하나 더 있는 것이므로 count
+    else if (maxSum < sum) { // 더 큰 합을 찾은 경우
+        maxSum = sum; // 최대값 갱신
+        count = 1; // count 초기화
+    }}
+
+if (maxSum == 0) console.log("SAD"); // 정답 출력
+else {
+    console.log(maxSum);
+    console.log(count);
+}
